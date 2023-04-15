81e92e4baa94a8a8b251c019fa46a659baa0dbce
Juggwow <p.wiriyar@gmail.com>
2023-04-15 19:58:26 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
 //import FormGroup_TreeTrimmingDocument from './components/FormGroup_TreeTrimmingDocument/FormGroup_TreeTrimmingDocument';
-import Loginpage from './components/Loginpage/Login';
+//import Loginpage from './components/Loginpage/Login';
 //import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 
 
+// Bring in the GoogleLogin
+// component from the library
+import { GoogleLogin } from '@react-oauth/google';
 function App() {
-
   return (
-    
-    <div>
-      <header>
-      </header>
-      <body>
-        
-
-        <Loginpage />
-        
+      <div className="App">
+          <GoogleLogin
+            onSuccess={credentialResponse => {
+              console.log(credentialResponse);
+            }}
           
-        
-      </body>
-    </div>
+            onError={() => {
+              console.log('Login Failed');
+            }}
+          
+          />
+      </div>
   );
 }
-
 
 
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// Bringing in the GoogleOAuthProvider from the package
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <GoogleOAuthProvider clientId="625444603857-4igpobrhuviu4c5abrsonvsdastn6qnq.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
+  </React.StrictMode>
 )
